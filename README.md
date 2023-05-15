02df72bf9cc2bf6a3f09d453e31358ca6548a700
ftmzhrsckldr <ftmzhrsckldr>
2023-05-15 19:43:16 +0300

    BE-1352 Repeato component added

---
+++ README.md
@@ -0,0 +1,17 @@
+# Appcircle _Repeato Test Runner_ component
+
+You can execute repeato tests before deploying your app build.
+
+## Required Inputs
+
+- `AC_REPEATO_CLI_VER`: Repeato CLI Version. Set the repeato CLI version compatible to your workspace tests.
+- `AC_REPEATO_WORKSPACE_DIR`: Workspace Path. Repeato test runner need workspace path for setting up the workspace before executing batch.
+- `AC_REPEATO_BATCH_ID`: Repeato Batch ID. Provide batch id for the tests execution.
+- `AC_REPEATO_LIC_KEY`: Repeato License Key. Provide license key for the tests execution.
+- `AC_REPEATO_LOG_LEVEL`: Log Level. Switch to DEBUG if you have troubles running your batches. This prints a lot of additional information to the log.
+
+## Output Variables
+
+- `AC_REPEATO_REPORT`: Repeato Batch Report Zip File. Repeato batch report of executed tests.
+- `AC_REPEATO_JUNIT_REPORT`: Repeato JUnit XML File. Repeato executed tests report in JUnit XML format.
+- `AC_TEST_RESULT_PATH`: Output Path. The directory where your Junit XML report will be written to.
---
+++ main.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "${AC_REPEATO_WORKSPACE_DIR}" ]
+then
+    echo 'Missing input: AC_REPEATO_WORKSPACE_DIR' >&2
+    exit 1
+fi
+
+if [ -z "${AC_REPEATO_BATCH_ID}" ]
+then
+    echo 'Missing input: AC_REPEATO_BATCH_ID' >&2
+    exit 1
+fi
+
+if [[ -z "$AC_REPEATO_CLI_VER" ]]; then
+    echo "Repeato CLI version not specified, using latest"
+else
+    echo "Repeato CLI version: $AC_REPEATO_CLI_VER"
+fi
+
+# Start repeato batch run tests & upload report
+rm -rf repeato-reports
+npm i -g @repeato/cli-testrunner@${AC_REPEATO_CLI_VER}
+cli-testrunner --licenseKey "${AC_REPEATO_LIC_KEY}" --workspaceDir "${AC_REPEATO_WORKSPACE_DIR}" --batchId "${AC_REPEATO_BATCH_ID}" --outputDir "${AC_OUTPUT_DIR}/repeato-test-results" --logLevel "${AC_REPEATO_LOG_LEVEL}"
+zip -r repeato_reports_$AC_REPEATO_BATCH_ID.zip $AC_OUTPUT_DIR/repeato-test-results
+cp repeato_reports_$AC_REPEATO_BATCH_ID.zip $AC_OUTPUT_DIR/batch_report_$AC_REPEATO_BATCH_ID.zip
+cp jUnitReport.xml $AC_OUTPUT_DIR/UnitTest.xml
+
+echo "AC_TEST_RESULT_PATH=$AC_OUTPUT_DIR/repeato-reports" >> $AC_ENV_FILE_PATH
+echo "AC_REPEATO_REPORT=$AC_OUTPUT_DIR/UnitTest.xml" >> $AC_ENV_FILE_PATH
+echo "AC_REPEATO_JUNIT_REPORT=$AC_OUTPUT_DIR/$AC_REPEATO_BATCH_ID.zip" >> $AC_ENV_FILE_PATH
